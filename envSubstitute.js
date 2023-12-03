f719bedb9b582572fc19eb6d67b827a3e65778bf
CodeMorty <CodeMorty@163.com>
2023-12-03 15:38:31 +0800

    在main提交

---
+++ envSubstitute.js
@@ -0,0 +1,24 @@
+//此文件是.env文件的说明文件,根据此文件自行创建.env文件,并填写相应的配置信息;
+// # 端口号;
+// APP_PORT=
+// # 邮箱配置;
+// # 邮箱;
+// YOUR_EMAIL_NAME=
+// # 邮箱生成的权限码;
+// YOUR_EMAIL_PASSWORD=
+
+// # 数据库配置;
+// # 数据库名;
+// MYSQL_DB=
+// # 数据库用户名;
+// MYSQL_USER=
+// # 数据库密码;
+// MYSQL_PASSWORD=
+// # 数据库端口号;
+// MYSQL_PORT=
+// # 数据库地址;
+// MYSQL_HOST=localhost
+// # 数据库类型;
+// MYSQL_TYPE=mysql
+// # token配置;
+// JWT_SECRET=
